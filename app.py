60bf8d9da2de97c52f62829a68359ab4ad454865
colm <mail@colm.be>
2015-03-08 16:14:00 +0100

    beginning of the login area

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-# Python Standard Library 
+# Python Standard Library
 import os
 from datetime import datetime
 
 # Dependencies: Flask + PIL or Pillow
-from flask import Flask, send_from_directory, redirect as redirect_flask, render_template
+from flask import Flask, send_from_directory, redirect as redirect_flask, render_template, url_for, request
 import pymongo
 
 # Local imports
@@ -30,6 +30,19 @@ def artists():
 def artist(slug):
     artist = db.artists.find_one({ "slug": slug})
     return render_template("artist.html", artist=artist)
+
+# route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
