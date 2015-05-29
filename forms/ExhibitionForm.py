051abc978af856899f985b5253346ac5393671fc
gijs <gijs@de-heij.com>
2015-05-29 18:07:32 +0200

    Better styling for admin form, added datepickers

--- forms/ExhibitionForm.py
+++ forms/ExhibitionForm.py
@@ -1,6 +1,8 @@
 from flask_wtf import Form
-from wtforms import StringField
+from wtforms import StringField, DateField
 from wtforms.validators import DataRequired
 
 class ExhibitionForm(Form):
     name = StringField('Artist name', validators=[DataRequired()])
+    start = DateField('From', validators=[DataRequired()], format='%d-%m-%Y')
+    end = DateField('To', validators=[DataRequired()], format='%d-%m-%Y')
