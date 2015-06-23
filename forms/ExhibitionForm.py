b2820bd48a7dfbbfaed7d9cb1cea4da78e96c384
colm <mail@colm.be>
2015-06-24 00:02:32 +0200

    adding fields to the exhibition form + some interface stuff &&&&&

--- forms/ExhibitionForm.py
+++ forms/ExhibitionForm.py
@@ -4,9 +4,16 @@ from wtforms import StringField, DateTimeField, TextAreaField, FileField
 from wtforms.validators import DataRequired
 
 class ExhibitionForm(Form):
-    name = StringField('Title', validators=[DataRequired()])
+    artist = StringField(u'Artist Name *', validators=[DataRequired()])
+    name = StringField('Title')
     start = DateTimeField('From', validators=[DataRequired()], format='%d-%m-%Y')
     end = DateTimeField('To', validators=[DataRequired()], format='%d-%m-%Y')
-    wysiwig_description = PageDownField('Enter your Markdown')
+    # key_image = FileField('Exhibition key image')
+    # other images = FileField('Other images from AL')
+    wysiwig_exhibition_description = PageDownField('exhibition description')
+    wysiwig_artist_bio = PageDownField('artist bio')
     press_release_file = FileField('Press release files')
+    # press_release_file_en = FileField('Press release files EN')
+    # press_release_file_fr = FileField('Press release files FR')
+    # press_release_file_nl = FileField('Press release files NL')
     # submit = SubmitField('Submit')
