d384beee13de3d949f0aea9915b8b5d505fb7daf
colm <mail@colm.be>
2015-08-11 10:47:13 +0200

    configs on the wtfs for proper field rendering passwords and all

--- forms/GroupExhibitionForm.py
+++ forms/GroupExhibitionForm.py
@@ -9,7 +9,7 @@ class GroupExhibitionForm(Form):
     start = DateTimeField('From', validators=[DataRequired()], format='%d-%m-%Y')
     end = DateTimeField('To', validators=[DataRequired()], format='%d-%m-%Y')
     exhibition_key_img = FileField('exhibition key image')
-    artists = SelectMultipleField(u'Select Artist *', option_widget=CheckboxInput(), validators=[DataRequired()])
+    artists = RadioField(u'Select Artist *', option_widget=CheckboxInput(), validators=[DataRequired()])
     extra_artists = StringField(u'Extra artists names (non gallery artists)')
     wysiwig_exhibition_description = PageDownField('exhibition description')
     press_release_file = FileField('Press release files')
--- forms/Login.py
+++ forms/Login.py
@@ -1,7 +1,7 @@
 from flask_wtf import Form
-from wtforms import SelectField, FileField, StringField
+from wtforms import SelectField, FileField, StringField, PasswordField
 from wtforms.validators import DataRequired
 
 class Login(Form):
     username = StringField(u'Username', validators=[DataRequired()])
-    password = StringField(u'Password', validators=[DataRequired()])
+    password = PasswordField(u'Password', validators=[DataRequired()])
