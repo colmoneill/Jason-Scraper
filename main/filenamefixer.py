12a0b13750a423bd4e673d75c29cf001ebec1c44
gijs <gijs@de-heij.com>
2015-09-30 13:21:54 +0200

    Test whether an exhibition has images first.

--- main/filenamefixer.py
+++ main/filenamefixer.py
@@ -7,11 +7,12 @@ from bson import ObjectId
 for exhibition in settings.db.exhibitions.find():
     if not exhibition['slug'] and 'is_group_expo' not in exhibition:
         filenamebase = exhibition['artist']['slug'] + '-' + exhibition['start'].strftime('%d-%m-%Y')
-
-        for key, image in enumerate(exhibition['images']):
-            newpath = utils.getsafepath('static/uploads/exhibition-view/' + filenamebase + '.jpg')
-            shutil.move(os.path.join(settings.appdir, image['path']), os.path.join(settings.appdir, newpath))
-            exhibition['images'][key]['path'] = newpath
+        
+        if 'images' in exhibition:
+            for key, image in enumerate(exhibition['images']):
+                newpath = utils.getsafepath('static/uploads/exhibition-view/' + filenamebase + '.jpg')
+                shutil.move(os.path.join(settings.appdir, image['path']), os.path.join(settings.appdir, newpath))
+                exhibition['images'][key]['path'] = newpath
         
         if 'press_release' in exhibition and exhibition['press_release']:
             newpath = utils.getsafepath('static/uploads/press/' + filenamebase + '.pdf')
