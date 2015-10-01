4faea3d12f4db27f6d8672371c935769dd66d232
gijs <gijs@de-heij.com>
2015-10-01 14:27:03 +0200

    added case to dealt with images which have no path.

--- main/filenamefixer.py
+++ main/filenamefixer.py
@@ -10,9 +10,10 @@ for exhibition in settings.db.exhibitions.find():
         
         if 'images' in exhibition:
             for key, image in enumerate(exhibition['images']):
-                newpath = utils.getsafepath('static/uploads/exhibition-view/' + filenamebase + '.jpg')
-                shutil.move(os.path.join(settings.appdir, image['path']), os.path.join(settings.appdir, newpath))
-                exhibition['images'][key]['path'] = newpath
+                if image['path'] and image['path'] <> 'undefined':
+                    newpath = utils.getsafepath('static/uploads/exhibition-view/' + filenamebase + '.jpg')
+                    shutil.move(os.path.join(settings.appdir, image['path']), os.path.join(settings.appdir, newpath))
+                    exhibition['images'][key]['path'] = newpath
         
         if 'press_release' in exhibition and exhibition['press_release']:
             newpath = utils.getsafepath('static/uploads/press/' + filenamebase + '.pdf')
