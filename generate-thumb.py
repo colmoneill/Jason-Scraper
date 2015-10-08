6b0c4437dec518c2d2b781849bfedd7958b7404b
colm <mail@colm.be>
2015-10-08 21:31:29 +0200

    ok it works but the paths are static, I'll have to do it manually for the covers also

--- generate-thumb.py
+++ generate-thumb.py
@@ -1,9 +1,3 @@
-import settings
-import utils
-import shutil
-import os.path
-import re
-from bson import ObjectId
 from PIL import Image
 import glob, os
 
@@ -11,25 +5,23 @@ infolder = "static/uploads/artworks"
 outfolder = "static/uploads/artworks/thumbs-reg/"
 outfolder2 = "static/uploads/artworks/thumbs-big/"
 
-
 for f in glob.glob(os.path.join(infolder, "*.jpg")):
     path, name = os.path.split(f)
     base, ext = os.path.splitext(name)
-    im = Image.open(f)
-    im.thumbnail((240, 160), Image.ANTIALIAS)
-    outpath_reg = os.path.join(outfolder, base+"-thumb240.jpg")
-    if not os.path.exists(outpath_reg):
-        print(outpath_reg)
-        im.save(outpath_reg)
-
-# for f in glob.glob(os.path.join(infolder, "*.jpg")):
-#     path, name = os.path.split(f)
-#     base, ext = os.path.splitext(name)
-#     im = Image.open(f)
-#     im.thumbnail((480, 320), Image.ANTIALIAS)
-#     outpath_big = os.path.join(outfolder, base+"-thumb480.jpg")
-#     image['big_thumb_path'] = outpath_big
-#     if not os.path.exists(outpath_big):
-#         print(outpath_big)
-#         im.save(outpath_big)
-#
+    try:
+        im = Image.open(f)
+        print "generating reg thumbnail" + (name)
+        im.thumbnail((240, 160), Image.ANTIALIAS)
+        outpath_reg = os.path.join(outfolder, base+"-thumb240.jpg")
+        if not os.path.exists(outpath_reg):
+            print(outpath_reg)
+            im.save(outpath_reg)
+        im = Image.open(f)
+        print "generating big thumbnail" + (name)
+        im.thumbnail((480, 320), Image.ANTIALIAS)
+        outpath_big = os.path.join(outfolder2, base+"-thumb480.jpg")
+        if not os.path.exists(outpath_big):
+            print(outpath_big)
+            im.save(outpath_big)
+    except IOError:
+        print "image corrupt; skipping"
