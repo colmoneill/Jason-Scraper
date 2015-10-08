21635fbbfc29574a4c4ff5ad91e05a5a03f3d006
colm <mail@colm.be>
2015-10-08 22:38:49 +0200

    I have to stop here for now

--- generate-thumb.py
+++ generate-thumb.py
@@ -1,3 +1,4 @@
+import settings
 from PIL import Image
 import glob, os
 
@@ -12,14 +13,15 @@ for f in glob.glob(os.path.join(infolder, "*.jpg")):
         im = Image.open(f)
         print "generating reg thumbnail" + (name)
         im.thumbnail((240, 160), Image.ANTIALIAS)
-        outpath_reg = os.path.join(outfolder, base+"-thumb240.jpg")
+        outpath_reg = os.path.join(outfolder, base+".jpg")
         if not os.path.exists(outpath_reg):
             print(outpath_reg)
             im.save(outpath_reg)
+            settings.db.image.insert(reg_thumb_path = outpath_reg)
         im = Image.open(f)
         print "generating big thumbnail" + (name)
         im.thumbnail((480, 320), Image.ANTIALIAS)
-        outpath_big = os.path.join(outfolder2, base+"-thumb480.jpg")
+        outpath_big = os.path.join(outfolder2, base+".jpg")
         if not os.path.exists(outpath_big):
             print(outpath_big)
             im.save(outpath_big)
