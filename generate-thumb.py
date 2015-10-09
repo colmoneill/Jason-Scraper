3bb69cc04de29fcbc2961efe57948e61d1f4fb88
colm <mail@colm.be>
2015-10-09 14:14:46 +0200

    trying to make this more legible

--- generate-thumb.py
+++ generate-thumb.py
@@ -6,20 +6,20 @@ infolder = "static/uploads/artworks"
 outfolder = "static/uploads/artworks/thumbs-reg/"
 outfolder2 = "static/uploads/artworks/thumbs-big/"
 
-for f in glob.glob(os.path.join(infolder, "*.jpg")):
-    path, name = os.path.split(f)
+for image in glob.glob(os.path.join(infolder, "*.jpg")):
+    path, name = os.path.split(image)
     base, ext = os.path.splitext(name)
     try:
-        im = Image.open(f)
-        print "generating reg thumbnail" + (name)
+        im = Image.open(image)
+        print "generating reg thumbnail " + (outfolder) + (name)
         im.thumbnail((240, 160), Image.ANTIALIAS)
         outpath_reg = os.path.join(outfolder, base+".jpg")
         if not os.path.exists(outpath_reg):
             print(outpath_reg)
             im.save(outpath_reg)
             settings.db.image.insert(reg_thumb_path = outpath_reg)
-        im = Image.open(f)
-        print "generating big thumbnail" + (name)
+        im = Image.open(image)
+        print "generating big thumbnail " + (outfolder2) + (name)
         im.thumbnail((480, 320), Image.ANTIALIAS)
         outpath_big = os.path.join(outfolder2, base+".jpg")
         if not os.path.exists(outpath_big):
