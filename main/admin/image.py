b735116f4d77198461aab8ef8202b9fabed91464
colm <mail@colm.be>
2015-10-27 12:10:11 +0100

    artist sort in listing

--- main/admin/image.py
+++ main/admin/image.py
@@ -19,7 +19,7 @@ def index():
     form.artist.choices = [(str(artist['_id']), artist['name']) for artist in db.artist.find().sort("artist_sort")]
     artists = db.artist.find({
     "is_published": True,
-    })
+    }).sort([("artist_sort", 1)])
     images = db.image.find().sort([("artist_sort", 1)])
     return render_template('admin/image/index.html', images=images, artists=artists, form=form)
 
