c611f24274b789980a6cff28fad8c01c81da87ea
colm <mail@colm.be>
2015-11-06 17:51:23 +0100

    a script to generate ids for all exhibition views inside each exhibition

---
+++ views_id_generation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from main.settings import db
+from bson import ObjectId
+
+for exhibition in db.exhibitions.find():
+    images = []
+
+    if 'images' in exhibition and type(exhibition['images']) is list:
+        for image in exhibition['images']:
+            if type(image) is dict:
+                image['_id'] = ObjectId()
+                images.append(image)
+    exhibition['images'] = images
+
+    db.exhibitions.update({ '_id': exhibition['_id'] }, exhibition)
