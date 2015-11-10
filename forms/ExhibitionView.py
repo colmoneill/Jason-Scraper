0e54fab4adac6c76f6b34de0468ca42695150f8f
colm <mail@colm.be>
2015-11-10 20:37:55 +0100

    reevaluation the index listings and info display + typo corrections and capitalisations etc

--- forms/ExhibitionView.py
+++ forms/ExhibitionView.py
@@ -3,8 +3,8 @@ from wtforms import SelectField, FileField, StringField
 from wtforms.validators import DataRequired
 
 class ExhibitionView(Form):
-    artist = StringField(u'artists visible in the view')
-    exhibition_title = StringField(u'exhibition views exhibition title')
-    year = StringField(u'year of the exhibition')
-    institution = StringField(u'institution')
+    artist = StringField(u'Artists visible in the view')
+    exhibition_title = StringField(u'Exhibition views exhibition title')
+    year = StringField(u'Year of the exhibition')
+    institution = StringField(u'Institution')
     country = StringField(u'Country')
