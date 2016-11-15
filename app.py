2baa88b3f946342a34e65e08ef93a212201cf9d4
gijs <gijs@de-heij.com>
2016-11-15 20:08:35 +0100

    Forgot one place to use settings.appdir

--- app.py
+++ app.py
@@ -360,7 +360,7 @@ def generateThumb(path):
     folder = os.path.normpath(folder)
 
     if os.path.exists(os.path.join(settings.appdir, 'static/uploads', folder)) and os.path.exists(os.path.join(settings.appdir, inpath)):
-        im = Image.open(inpath)
+        im = Image.open(os.path.join(settings.appdir, inpath))
         im.thumbnail((240, 160), Image.ANTIALIAS)
         im.save(os.path.join(settings.appdir, 'static/thumbs', path))
         return send_from_directory(os.path.join(settings.appdir, 'static/thumbs'), path)
