5be46516244ca57142908401dbf2aa345015fb4f
colm <mail@colm.be>
2020-04-28 18:47:36 +0100

    start of pricing #23

--- forms/Image.py
+++ forms/Image.py
@@ -1,21 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from flask_wtf import Form
 from wtforms import SelectField, FileField, StringField
 from wtforms.validators import DataRequired
+example_price = "€1.000".decode("utf-8")
 
 class Image(Form):
     artist = SelectField(u'Select artist *', validators=[DataRequired()])
     image_file = FileField(u'Image to upload *', validators=[DataRequired()])
-    title = StringField(u'title of the artwork')
+    title = StringField(u'Title of the artwork')
     stock_number = StringField(u'Stock number')
-    year = StringField(u'year')
+    year = StringField(u'Year')
     medium = StringField(u'Artwork medium')
-    dimensions = StringField(u'Artwork dimentions in CM')
-    edition = StringField(u'artwork edition info')
+    dimensions = StringField(u'Artwork dimentions (include unit)' , render_kw={"placeholder": "depth x width x height CM"})
+    edition = StringField(u'Artwork edition info')
+    price = StringField(u'Artwork price (include currency symbol)', render_kw={"placeholder": example_price})
 
 class ImageUpdate(Form):
-    title = StringField(u'title of the artwork')
+    title = StringField(u'Title of the artwork')
     stock_number = StringField(u'Stock number')
-    year = StringField(u'year')
+    year = StringField(u'Year')
     medium = StringField(u'Artwork medium')
-    dimensions = StringField(u'Artwork dimentions in CM')
-    edition = StringField(u'artwork edition info')
+    dimensions = StringField(u'Artwork dimentions (include unit)', render_kw={"placeholder": "depth x width x height CM"})
+    edition = StringField(u'Artwork edition info')
+    price = StringField(u'Artwork price (include currency symbol)', render_kw={"placeholder": example_price})
