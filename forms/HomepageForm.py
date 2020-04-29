290d48a2d0eaf35bf3a5dfc07699987518e4e715
colm <mail@colm.be>
2020-04-29 20:28:52 +0100

    changing the order of ops here or the lists do not update until server restarts

--- forms/HomepageForm.py
+++ forms/HomepageForm.py
@@ -1,15 +1,25 @@
 from flask_wtf import Form
 from flask_pagedown.fields import PageDownField
-from wtforms import StringField, FileField, TextAreaField, RadioField
+from wtforms import StringField, FileField, TextAreaField, RadioField, SelectField
 from wtforms.validators import DataRequired
+
+from main.settings import db
+from bson import ObjectId
+
+ext_exhibition = db.exhibitions.find({
+    "is_published": True,
+    "$or": [
+        {"location": "external"},
+        {"location": "virtual"},
+    ]
+    })
 
 class HomepageForm(Form):
     status = RadioField(u'Enable special homepage layout ?', choices = [
-    ('no', 'no, use normal side by side exhibition layout, option 1'),
-    ('yes', 'yes, using option 2, highlight only the show at *32* Livornostraat'),
-    ('yes', 'yes, using option 3, highlight only the show at *35* Livornostraat'),
-    ('yes', 'yes, using option 4, highlight an external show'),
-    ('yes', 'yes, using option 5, triptych — show exhibitions in showrooms 32, 35 and a selected third exhibition'),
-    ],validators=[DataRequired()])
-    status = RadioField(u'Choose which external exhibition to highlight : ', choices = [])
+    ('opt1', 'no, use normal side by side exhibition layout, option 1'),
+    ('opt2', 'yes, using option 2, highlight only the show at *32* Livornostraat'),
+    ('opt3', 'yes, using option 3, highlight only the show at *35* Livornostraat'),
+    ('opt4', 'yes, using option 4, highlight an external show'),
+    ('opt5', 'yes, using option 5, triptych - show exhibitions in showrooms 32, 35 and a selected third exhibition')])
+    choosen_ext_exhibition_id = SelectField(choices=[(g['_id'], g['exhibition_name']) for g in ext_exhibition], coerce=ObjectId, label="Choose which external exhibition to highlight : ")
     internal_link = StringField('paste here the link of the page users should get to when they click on this homepage element')
--- main/admin/homepage_management.py
+++ main/admin/homepage_management.py
@@ -14,35 +14,33 @@ from flask_pagedown.fields import PageDownField
 from wtforms import StringField, FileField, TextAreaField, RadioField, SelectField
 from wtforms.validators import DataRequired
 
-ext_exhibition = db.exhibitions.find({
-    "is_published": True,
-    "$or": [
-        {"location": "external"},
-        {"location": "virtual"},
-    ]
-    })
-
-
-class HomepageForm(Form):
-    status = RadioField(u'Enable special homepage layout ?', choices = [
-    ('opt1', 'no, use normal side by side exhibition layout, option 1'),
-    ('opt2', 'yes, using option 2, highlight only the show at *32* Livornostraat'),
-    ('opt3', 'yes, using option 3, highlight only the show at *35* Livornostraat'),
-    ('opt4', 'yes, using option 4, highlight an external show'),
-    ('opt5', 'yes, using option 5, triptych - show exhibitions in showrooms 32, 35 and a selected third exhibition')])
-    choosen_ext_exhibition_id = SelectField(choices=[(g['_id'], g['exhibition_name']) for g in ext_exhibition], coerce=ObjectId, label="Choose which external exhibition to highlight : ")
-    internal_link = StringField('paste here the link of the page users should get to when they click on this homepage element')
 
 blueprint = Blueprint('homepage_management', __name__)
 
 @blueprint.route("/", methods=['GET','POST'])
 @login_required
 def homepage_status():
-    form = HomepageForm()
-    # form.choosen_ext_exhibition_id.choices = [(g['_id'], g['exhibition_name']) for g in ext_exhibition]
     homepage_info = db.homepage.find_one({"_id": 'current_status'})
-    print("choices:")
-    print(form.choosen_ext_exhibition_id.choices)
+
+    ext_exhibition = db.exhibitions.find({
+        "is_published": True,
+        "$or": [
+            {"location": "external"},
+            {"location": "virtual"},
+        ]
+        })
+
+    class HomepageForm(Form):
+        status = RadioField(u'Enable special homepage layout ?', choices = [
+        ('opt1', 'no, use normal side by side exhibition layout, option 1'),
+        ('opt2', 'yes, using option 2, highlight only the show at *32* Livornostraat'),
+        ('opt3', 'yes, using option 3, highlight only the show at *35* Livornostraat'),
+        ('opt4', 'yes, using option 4, highlight an external show'),
+        ('opt5', 'yes, using option 5, triptych - show exhibitions in showrooms 32, 35 and a selected third exhibition')])
+        choosen_ext_exhibition_id = SelectField(choices=[(g['_id'], g['exhibition_name']) for g in ext_exhibition], coerce=ObjectId, label="Choose which external exhibition to highlight : ")
+        internal_link = StringField('paste here the link of the page users should get to when they click on this homepage element')
+
+    form = HomepageForm()
 
     if form.validate_on_submit():
         formdata = form.data
@@ -50,27 +48,3 @@ def homepage_status():
         db.homepage.update({"_id":'current_status'},homepage_status, upsert=True)
         flash(u'You updated the homepage layout', 'success')
     return render_template('admin/homepage-management/index.html', ext_exhibition=ext_exhibition, form=form, homepage_info=homepage_info)
-
-
-# str_choices = [(ext_exhibition[0]['_id'], ext_exhibition[0]['exhibition_name']),]
-# str_choices = [('none', 'you are seeing this text because no exhibitions have been set to external')]
-#
-# quantity_ext_exhbition = db.exhibitions.find({
-#     "is_published": True,
-#     "location": "external",
-#     }).count()
-# print("amount of external exhibitions: ")
-# print(quantity_ext_exhbition)
-# if quantity_ext_exhbition == 1 :
-#     str_choices = [(ext_exhibition[0]['_id'], ext_exhibition[0]['exhibition_name']),]
-# if quantity_ext_exhbition == 2 :
-#     str_choices = [
-#     (ext_exhibition[0]['_id'], ext_exhibition[0]['exhibition_name']),
-#     (ext_exhibition[1]['_id'], ext_exhibition[1]['exhibition_name']),
-#     ]
-# if quantity_ext_exhbition == 3:
-#     str_choices = [
-#     (ext_exhibition[0]['_id'], ext_exhibition[0]['exhibition_name']),
-#     (ext_exhibition[1]['_id'], ext_exhibition[1]['exhibition_name']),
-#     (ext_exhibition[2]['_id'], ext_exhibition[2]['exhibition_name']),
-#     ]
